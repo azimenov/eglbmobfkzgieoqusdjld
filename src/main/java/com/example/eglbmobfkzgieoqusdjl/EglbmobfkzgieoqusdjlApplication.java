c9ea7dc42b20408648164b76699db8de8f164ca3
margulan <dynastymm9@gmail.com>
2024-02-08 17:55:31 +0600

    init changed gradle version to 7.6

---
+++ src/main/java/com/example/eglbmobfkzgieoqusdjl/EglbmobfkzgieoqusdjlApplication.java
@@ -0,0 +1,13 @@
+package com.example.eglbmobfkzgieoqusdjl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EglbmobfkzgieoqusdjlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EglbmobfkzgieoqusdjlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eglbmobfkzgieoqusdjl/EglbmobfkzgieoqusdjlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eglbmobfkzgieoqusdjl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EglbmobfkzgieoqusdjlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
